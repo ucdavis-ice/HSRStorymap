5c3fa95d0f3e3e180700b62525ca75bda221974a
nathanielroth <nathanielroth@gmail.com>
2014-06-23 16:30:42 -0700

    Move to Map v2

--- app/series/Config.js
+++ app/series/Config.js
@@ -7,7 +7,7 @@ define([],
 			//The web map id
 			webmaps: [
 			{
-				id: "b9c496675c02461cbdc8e3cd850976e3",
+				id: "40fb83b037824117a323d14dd52d8670",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -16,7 +16,7 @@ define([],
 				// Set to true if you want the legend of this specific map to open when a user selects the tab for this map.
 				openLegendOnChange: false
 			},{
-				id: "a01927e7557f41dc8eef93c8820e6763",
+				id: "ffe596cdd81542dba171f1f2f65321b7",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -26,7 +26,7 @@ define([],
 				openLegendOnChange: false
 			},
 			{
-				id: "66d03eaca55443c7998e55a571704966",
+				id: "cb052044b12e4f359b03079bf40b5275",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -36,7 +36,27 @@ define([],
 				openLegendOnChange: false
 			},
 			{
-				id: "e943dca78cd941e1b33e2a8ed3de3d47",
+				id: "d2182638ca324bd2b2730c22dc2f061e",
+				title: "",
+				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
+				showSingleTimeInstance: false,
+				// Hide legend and legend toggle for specific map by setting this option to false
+				legendVisible: true,
+				// Set to true if you want the legend of this specific map to open when a user selects the tab for this map.
+				openLegendOnChange: false
+			},
+			{
+				id: "dc8001524da7429da2b7b20fb86988af",
+				title: "",
+				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
+				showSingleTimeInstance: false,
+				// Hide legend and legend toggle for specific map by setting this option to false
+				legendVisible: true,
+				// Set to true if you want the legend of this specific map to open when a user selects the tab for this map.
+				openLegendOnChange: false
+			},
+			{
+				id: "ffdeab128d1f4df5945606b0f0482c8c",
 				title: "",
 				// If your map has time properties, choose to show a single time instance instead of the time interval saved with the web map.
 				showSingleTimeInstance: false,
@@ -71,7 +91,7 @@ define([],
 			//	}
 			],
 			//Enter a title, if no title is specified, the first webmap's title is used.
-			title: "California High Speed Rail",
+			title: "California High Speed Rail (v2)",
 			//Enter a subtitle, if no subtitle is specified, the first webmap's subtitle is used.
 			subtitle: "Benefits to California",
 			// If false, each tab will have a number on it. If true, the first tab will not have a number and the second tab will start counting at 1.
